a0626ef655308603fdd0eb95d1879197b34d1df2
xuetrdi <xuetr_di@icloud.com>
2020-01-13 14:24:55 +0800

    initial

---
+++ clean_latex_tmp_file.sh
@@ -0,0 +1,8 @@
+rm -rf $HOME/doc/latex/*.out
+rm -rf $HOME/doc/latex/*.aux
+rm -rf $HOME/doc/latex/*.gz
+rm -rf $HOME/doc/latex/*.toc
+rm -rf $HOME/doc/latex/*.snm
+rm -rf $HOME/doc/latex/*.log
+rm -rf $HOME/doc/latex/*.nav
+
---
+++ uninstall_and_clean_vagrant.sh
@@ -0,0 +1,5 @@
+sudo rm -rf /opt/vagrant
+sudo rm -f /usr/local/bin/vagrant
+sudo pkgutil --forget com.vagrant.vagrant
+rm -rf ~/.vagrant
+rm -rf ~/.vagrant.d
